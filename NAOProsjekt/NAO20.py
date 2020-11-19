93b44bad6d7b97408716407553cef02e8299aff1
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2020-11-19 11:41:45 +0100

    Update NAO20.py

--- NAOProsjekt/NAO20.py
+++ NAOProsjekt/NAO20.py
@@ -1,9 +1,10 @@
 
 import sys
-sys.path.append("C:\Users\Legendary\Documents\ELVE3610\Prosjekt\pynaoqi-python2.7-2.8.6.23\pynaoqi-python2.7-2.8.6.23-win64-vs2015-20191127_152649\lib")
+PATH = 'path/to/NAO/library'
+sys.path.append(PATH)
 from naoqi import ALProxy
 
-naoIP = #"192.168.12.62"
+naoIP = "127.0.0.0"
 PORT = 9559
 
 def setupNAO(name):
@@ -16,14 +17,14 @@ positur = setupNAO("ALRobotPosture")
 
 snakke.say("Hello. Do you want to play?")
 #Sett inn voice recognition
-'''
+
 move.post.moveTo(0.5,-0.12,-0.28) #.post gjor at man kan gjore flere ting samtidig
 #move.moveToward(1,0,0)
 snakke.say("I am walking one meter")
 move.waitUntilMoveIsFinished()
 snakke.say("Done walking")
 positur.goToPosture("Sit",1.0)
-'''
+
 
 print(move.__dict__) #hva move objektet inneholder
 #print(dir(move))
