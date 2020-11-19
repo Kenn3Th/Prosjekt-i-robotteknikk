1484f966017377311e5e3977891ff2d0b9acdadc
unknown <erik_joha@live.com>
2020-11-19 16:27:47 +0100

    oppdatert python

--- ABB_Prosjekt/center_contour.py
+++ ABB_Prosjekt/center_contour.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
+import time
 
-test = 2
 
 frameWidth = 1680    
 frameHight = 1050
@@ -9,7 +9,9 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHight)
 
-test = 3
+
+
+
 
 def empty(a):
     pass
@@ -59,8 +61,8 @@ def stackImages(scale,imgArray):
 def getContours(img,imgContour):
 
 
-    contours = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    edges = 0
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
@@ -76,38 +78,47 @@ def getContours(img,imgContour):
 		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
-            #print(len(approx))
-            print(cX,cY)
             x,y,w,h = cv2.boundingRect(approx)
             cv2.rectangle(imgContour, (x,y), (x + w, y + h), (0,255,0), 5)
-
             cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
             cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
+            #ObjectAnalysis(len(approx))
+            edges = len(approx)
+    return edges
+
+def ObjectAnalysis(edges):
+    if edges == 3:
+        print(shape[1])
+    elif edges == 4:
+        print(shape[0])
+    elif edges == 6:
+        print(shape[2])
 
 
 
 
 
+if __name__ == "__main__":
 
-while True:
-    success, img = cap.read()
-    imgContour = img.copy()
+    while True:
+        success, img = cap.read()
+        imgContour = img.copy()
 
-    imgBlur = cv2.GaussianBlur(img,(7,7),1)
-    imgGray = cv2.cvtColor(imgBlur,cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(img,(7,7),1)
+        imgGray = cv2.cvtColor(imgBlur,cv2.COLOR_BGR2GRAY)
 
-    threshhold1 = cv2.getTrackbarPos("Threshhold1","Parameters")
-    threshhold2 = cv2.getTrackbarPos("Threshhold2","Parameters")
-    imgCanny = cv2.Canny(imgGray,threshhold1,threshhold2)
-    kernel = np.ones((5,5))
-    imgDil = cv2.dilate(imgCanny,kernel,iterations=1)
+        threshhold1 = cv2.getTrackbarPos("Threshhold1","Parameters")
+        threshhold2 = cv2.getTrackbarPos("Threshhold2","Parameters")
+        imgCanny = cv2.Canny(imgGray,threshhold1,threshhold2)
+        kernel = np.ones((5,5))
+        imgDil = cv2.dilate(imgCanny,kernel,iterations=1)
+        figur = getContours(imgDil,imgContour)
+        
 
-    getContours(imgDil,imgContour)
+        imgStack = stackImages(0.8,([imgContour]))
 
-
-
-    imgStack = stackImages(0.8,([imgContour]))
-
-    cv2.imshow("Result",imgStack)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow("Result",imgStack)
+        ObjectAnalysis(figur)
+        time.sleep(2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
